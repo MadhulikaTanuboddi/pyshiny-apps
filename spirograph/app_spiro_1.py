06e7c7576f1cb48d4360ec0e1e1aa53345c2d9d4
MadhulikaTanuboddi <mdhlk88@gmail.com>
2022-09-11 18:27:34 -0700

    Spirograph app
    Adding a py-shiny app that generates a spirograph plot based on 3 input values. Using reactive function (with math operations) that returns points to be used in the output function.
    
    Ref: https://stackoverflow.com/questions/21946455/homework-help-for-making-a-spirograph

---
+++ spirograph/app_spiro_1.py
@@ -0,0 +1,46 @@
+from shiny import App, reactive, render, ui
+from math import radians, sin, cos
+
+# Import modules for plot rendering
+import matplotlib.pyplot as plt
+
+app_ui = ui.page_fluid(
+    ui.layout_sidebar(
+        ui.panel_sidebar(
+            ui.input_slider("start", "Start Angle", 0, 10, 1, step=None, ticks=False),
+            ui.input_slider("step", "Step Angle", 10, 100, 10),
+            ui.input_slider("origin", "Origin", min=0, max=50, value=(0, 1)),
+        ),
+        ui.panel_main(
+            ui.output_plot("plot"),
+        ),
+    ),
+)
+
+def server(input, output, session):
+    @reactive.Calc
+    def calculate_points():
+        points = []
+        values = (input.origin()[0], input.origin()[1])
+        points.append(values)
+        points.append((cos(radians(input.start())), sin(radians(input.start()))))
+
+        for i in range(input.start() + input.step(), 360 + input.step(), input.step()):
+            x = cos(radians(i))
+            y = sin(radians(i))
+            points.append((x,y))
+            points.append(values)
+            points.append((x,y))
+
+        x,y = zip(*points) # separate the tuples into x and y coordinates.
+
+        return x,y
+
+
+    @output
+    @render.plot
+    def plot():
+        a, b = calculate_points()
+        plt.plot(a, b) #plots the points, drawing lines between each point
+
+app = App(app_ui, server, debug=True)
