8486d254ff705732a5cff1fe700b8b6857633a63
MadhulikaTanuboddi <mdhlk88@gmail.com>
2022-10-25 17:47:09 -0700

    Dragon realm app - Adding more logic to tab navigation

--- dragon-realm-game/app.py
+++ dragon-realm-game/app.py
@@ -1,4 +1,4 @@
-from shiny import ui, reactive, render, App
+from shiny import *
 from typing import List
 from shiny.types import NavSetArg
 import random
@@ -21,7 +21,7 @@ def nav_controls() -> List[NavSetArg]:
             ui.input_action_button("cave_02", "Cave 2"),
         ),
         ui.nav("Consequences", 
-            ui.output_text("txt"),
+            ui.output_text("consequence_txt"),
 
             ui.h1("play again??"),
 
@@ -38,40 +38,15 @@ app_ui = ui.page_navbar(
     id = "dragon_tabs",
 
     footer=ui.div(
-        ui.navset_pill(*nav_controls())
+        # TODO: Implemenet hidden tabs just like R's tabsetpanel
+        # https://shiny.rstudio.com/reference/shiny/0.14/tabsetPanel.html
+        # See how to use navset_hidden
+        ui.navset_pill(*nav_controls(), id = "dragon_tabs"),
+        #ui.navset_hidden(*nav_controls(), id = "dragon_tabs")
     )
     
 
 ) 
-
-
-# # Baseline code
-# app_ui = ui.page_fluid(
-#     ui.h1("Intro"),
-#     ui.p(
-#         """
-#         You are in a land full of dragons. In front of you, you see two caves.
-#         In one cave, the dragon is friendly and will share his treasure with you. 
-#         The other dragon is greedy and hungry, and will eat you on sight
-#         """
-#     ),
-
-#     ui.input_action_button("intro_continue", "Continue"),
-    
-#     ui.h1("Question"),
-#     ui.p("Which cave you want to go into? (1 or 2)"),
-#     ui.input_action_button("cave_01", "Cave 1"),
-#     ui.input_action_button("cave_02", "Cave 2"),
-
-#     ui.h1("Consequences"),
-#     ui.output_text("txt"),
-
-#     ui.h1("play again??"),
-
-#     ui.input_action_button("play_again", "Play again!"),
-#     ui.input_action_button("end_game", "No, thank you")
-# )
-
 
 def consequences(cave_number: int):
     # friendly dragon == match with cave_number
@@ -90,9 +65,19 @@ def server(input, output, session):
     @reactive.event(input.intro_continue)
     def _():
         print("Intro continue button is pressed")
+        ui.update_navs("dragon_tabs", selected = "Question")
+
 
     cave_number = reactive.Value(1)
+
+    @reactive.Effect
+    @reactive.event(input.cave_01, input.cave_02, ignore_init = True)
+    def _():
+        print("A cave button was pressed")
+        ui.update_navs("dragon_tabs", selected = "Consequences")
     
+    
+    # BEFORE
     @reactive.Effect
     @reactive.event(input.cave_01)
     def _():
@@ -109,16 +94,27 @@ def server(input, output, session):
     @reactive.event(input.play_again)
     def _():
         print("Play again button is pressed")
+        ui.update_navs("dragon_tabs", selected = "Intro")
 
+    
     @reactive.Effect
     @reactive.event(input.end_game)
-    def _():
+    async def _():
         print("No, Thank you button is pressed")
-
+        await session.close()
+        
     @output
     @render.text
     def txt():
         return consequences(cave_number())
+
+    @output
+    @render.text
+    def consequence_txt():
+        list = [input.cave_01, input.cave_02]
+        print("hello")
+        return consequences(1)
+        #return consequences(cave_number())
     
 app = App(app_ui, server)
 
